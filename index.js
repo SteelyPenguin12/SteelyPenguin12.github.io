c9baeaf869745568b3290c05e0fb06fab57294df
SteelyPenguin12 <codkehler@outlook.com>
2025-12-27 22:48:15 -0600

    Refined Implementation of theme switch. made single toggle instead of 2 buttons, and added local storage of theme preference (overrides browser match preference)

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 //console.log(`variable insert into text like this $(variable)`);
 
 //variables
+const storageKey = 'theme-preference'; //key for local storage of most recent theme preference
 const theme = {
-    value: 'light',
+    value: getThemePreference(),
 }
-
 
 const rootElement = document.documentElement;
 const computedStyles = window.getComputedStyle(rootElement);
@@ -18,23 +18,22 @@ const secondaryLight = computedStyles.getPropertyValue('--secondary-light');
 const secondaryDark = computedStyles.getPropertyValue('--secondary-dark');
 
 //stores refrences to html objects
-const sunButton = document.getElementById("theme-button-sun");
-const moonButton = document.getElementById("theme-button-moon");
+const sunIcon = document.getElementById("theme-icon-sun");
+const moonIcon = document.getElementById("theme-icon-moon");
+const themeButton = document.getElementById("theme-button");
 
 //theme toggle functionality
-sunButton.addEventListener("click",function(){
-    sunButton.style.setProperty("background-color","white")
-    moonButton.style.setProperty("background-color","black")
-    theme.value = 'light'
-
-    reflectPreference();
-});
-moonButton.addEventListener("click",function(){
-    moonButton.style.setProperty("background-color","white")
-    sunButton.style.setProperty("background-color","black")
-    theme.value = 'dark'
-
-    reflectPreference();
+themeButton.addEventListener("click",function(){
+    if (theme.value === 'light'){
+        theme.value = 'dark'
+        sunIcon.style.setProperty("background-color",themeToggleDisabledColour);
+        moonIcon.style.setProperty("background-color",themeToggleEnabledColour);   
+    }else{
+        theme.value = 'light'
+        sunIcon.style.setProperty("background-color",themeToggleEnabledColour);
+        moonIcon.style.setProperty("background-color",themeToggleDisabledColour);
+    }
+    setThemePreference();
 });
 
 //functions
@@ -52,16 +51,28 @@ function reflectPreference() { //updates css to reflect selected theme preferenc
     }
 }
 
+function getThemePreference() {//figures out what the current theme preference should be
+    //checks local storage for saved preference first
+    if (localStorage.getItem(storageKey)) {
+        return localStorage.getItem(storageKey);
+    }
+    //then looks at browser/system preference if no local storage preference found
+    else{
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'dark';
+        }
+        else {
+            return 'light';
+        }
+    }
+}
+function setThemePreference() { //stores current theme preference to local storage
+    localStorage.setItem(storageKey, theme.value);
+    reflectPreference();
+}
+
 //runs on load and sets initial theme
 window.onload = () => {
-    //checks system preference for light or dark mode
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        console.log("Dark mode is preferred.");
-        theme.value = 'dark';
-    } else {
-        console.log("Light mode is preferred.");
-        theme.value = 'light';
-    }
     reflectPreference()
 
     //auto add onclick to correct buttons based on class
@@ -71,17 +82,19 @@ window.onload = () => {
     .addEventListener('click', onClick)*/
 };
 
-//CONSIDER!!!! whether this is necessary if we already have a button to manually toggle themes
 //adds an event listener to detect system theme changes
+//when browser preference changes, this also updates the locally stored preference
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (event) => {
   if (event.matches) {
-    // User switched to dark mode
+    // browser switched to dark mode
     console.log("System switched to dark mode!");
     theme.value = 'dark';
+    setThemePreference();
   } else {
-    // User switched to light mode
+    // browser switched to light mode
     console.log("System switched to light mode!");
     theme.value = 'light';
+    setThemePreference();
   }
   reflectPreference();
 });
